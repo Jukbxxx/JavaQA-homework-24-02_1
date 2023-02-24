878ddcca666d1357eb17456472b78e4ec59e0b6f
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-24 15:19:30 +0300

    .gitignore and project edited

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketCost = 1000; // Переменная, в которой хранится стоимость билета
+        int ticketCost = 13676; // Переменная, в которой хранится стоимость билета
         int milePerCost = 20; // Переменная, в которой хранится количество рублей для одной бонусной мили
         int mileForBonus = ticketCost / milePerCost; // Переменная, в которой хранится ответ по формуле расчета бонусов
         System.out.println("Мили к начислению: " + mileForBonus);
